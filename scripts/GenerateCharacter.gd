de81c6a8cae84b927fae532e1113a0b48215c778
BuildTools <unconfigured@null.spigotmc.org>
2021-01-31 00:26:25 +0000

    Expanded the names & Added new hat icons + base

--- scripts/GenerateCharacter.gd
+++ scripts/GenerateCharacter.gd
@@ -17,56 +17,49 @@ func _on_Button_pressed():
 	
 	#fully generates a character for said player
 func generateCharacter():
-	var feat1 = rng.randi_range(1, 5) #features for overlapping 
-	var feat2 = rng.randi_range(1, 5)
+	var feat1 = rng.randi_range(0, 6) #features hat
+	var feat2 = rng.randi_range(0, 5) #features face
 	var alignment = rng.randi_range(1, 2) #1 is innocent and 2 is criminal, this should be divided by playercount
 	var roleTrait #trait determined whether a player is innocent or not
 	var roleTraitDesc #description for above role
 	
 	#make sure features dont overlap (currently broken)
-	if feat1 == feat2:
-		if feat1 == 5:
-			feat1 -= 1
-		elif feat1 == 1:
-			feat1 += 1
-		else:
-			feat1 += 1
-	elif feat1 == 3 and feat2 == 4:
-			feat1 -= 1
-	elif feat1 == 4 and feat2 == 3:
-			feat1 += 1
 	
-	var firstName = CharacterData.first_names[rng.randi_range(0, 9)]
-	var lastName = CharacterData.last_names[rng.randi_range(0, 9)]
+	var firstName = CharacterData.first_names[rng.randi_range(0, CharacterData.first_names.size() - 1)]
+	var lastName = CharacterData.last_names[rng.randi_range(0, CharacterData.last_names.size() - 1)]
 		
 	#set the Name and Last name of the person
 	get_node("TextureRect/Name").text = firstName + " " + lastName
 		#decide feature 1
-	if feat1 == 1:
-		feat1 = load("res://assets/images/playericons/backpack.png")
+	if feat1 == 0:
+		feat1 = load("res://assets/images/newPlayerIcons/Hat_0.png")
+	elif feat1 == 1:
+		feat1 = load("res://assets/images/newPlayerIcons/Hat_1.png")
 	elif feat1 == 2:
-		feat1 = load("res://assets/images/playericons/cowboy.png")
+		feat1 = load("res://assets/images/newPlayerIcons/Hat_2.png")
 	elif feat1 == 3:
-		feat1 = load("res://assets/images/playericons/roundglasses.png")
+		feat1 = load("res://assets/images/newPlayerIcons/Hat_3.png")
 	elif feat1 == 4:
-		feat1 = load("res://assets/images/playericons/squareglasses.png")
+		feat1 = load("res://assets/images/newPlayerIcons/Hat_4.png")
+	elif feat1 == 5:
+		feat1 = load("res://assets/images/newPlayerIcons/Hat_5.png")
 	else:
-		feat1 = load("res://assets/images/playericons/smoking.png")
+		feat1 = null
 		#decide feature 2
-	if feat2 == 1:
-		feat2 = load("res://assets/images/playericons/backpack.png")
-	elif feat2 == 2:
-		feat2 = load("res://assets/images/playericons/cowboy.png")
-	elif feat2 == 3:
-		feat2 = load("res://assets/images/playericons/roundglasses.png")
-	elif feat2 == 4:
-		feat2 = load("res://assets/images/playericons/squareglasses.png")
-	else:
-		feat2 = load("res://assets/images/playericons/smoking.png")
+	#if feat2 == 1:
+	#	feat2 = load("res://assets/images/newPlayerIcons/backpack.png")
+	#elif feat2 == 2:
+	#	feat2 = load("res://assets/images/newPlayerIcons/cowboy.png")
+	#elif feat2 == 3:
+	#	feat2 = load("res://assets/images/newPlayerIcons/roundglasses.png")
+	#elif feat2 == 4:
+	#	feat2 = load("res://assets/images/newPlayerIcons/squareglasses.png")
+	#else:
+	#	feat2 = load("res://assets/images/newPlayerIcons/smoking.png")
 		
 		#set the feature textures
 	get_node("TextureRect/feature1").texture = feat1
-	get_node("TextureRect/feature2").texture = feat2
+	#get_node("TextureRect/feature2").texture = feat2
 	
 	#decide the alignment and the roleTrait
 	if alignment == 1:
